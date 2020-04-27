a6acfdb066af29c5bb99dc0245bf89d4c4579eeb
Fillipe Celestino <fillipecelestino@hotmail.com>
2020-04-27 11:37:21 -0300

    Criado array CATEGORIAS e criado as condicionais para definir categoria do competidor

--- script.php
+++ script.php
@@ -1,3 +1,25 @@
 <?php
 
-	
+	$categorias = [];
+	$categorias = ["infantil"];
+	$categorias = ["adolescente"];
+	$categorias = ["adulto"];
+
+
+	var_dump($categorias);
+
+	$nome = 'Fillipe';
+	$idade = 32;
+
+	if($idade >= 6 && $idade <= 12){
+		echo "O competidor ".$nome." pertence a categoria infantil";
+	}
+	elseif ($idade > 12 && $idade <= 18) {
+		echo "O competidor ".$nome." pertence a categoria adolescente";
+	}
+	elseif ($idade > 18 && $idade <= 60) {
+		echo "O competidor ".$nome." pertence a categoria adulto";
+	}
+	else{
+		echo "Infelizmente não pode participar!";
+	}
