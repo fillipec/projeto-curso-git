36b5e5f072a749bb1fd841bedd6d57c56da8b7ce
Fillipe Celestino <fillipecelestino@hotmail.com>
2020-04-27 16:23:47 -0300

    Validando entrada de dados

--- script.php
+++ script.php
@@ -10,6 +10,27 @@
 
 	$nome = $_POST['nome'];
 	$idade = $_POST['idade'];
+	//var_dump($nome);
+	//var_dump($idade);
+	//return 0;
+	if(empty($nome) || empty($idade)){
+		echo "Preencha todos os dados";
+		return;
+	}
+	if (strlen($nome) < 5) {
+		echo "Digite o nome completo";
+		return;
+	}
+
+	if (strlen($nome) > 40) {
+		echo "Nome muito extenso";
+		return;
+	}
+
+	if(!is_numeric($idade)){
+		echo "Idade invalida";
+		return;
+	}
 
 	if($idade >= 6 && $idade <= 12){
 		echo "O competidor ".$nome." pertence a categoria infantil";
