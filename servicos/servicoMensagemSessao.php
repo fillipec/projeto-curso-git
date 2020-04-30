84740613dd2148673f5e15a86ce7c011f9b5ab83
Fillipe Celestino <fillipecelestino@hotmail.com>
2020-04-30 11:24:42 -0300

    criando funcoes nos servicos mensagem sucesso, erro e remover essas mensagem

--- servicos/servicoMensagemSessao.php
+++ servicos/servicoMensagemSessao.php
@@ -3,6 +3,25 @@
 	session_start();
 
 
+	function setMensagemSucesso(string $mensagem) :void
+	{
+		
+		$_SESSION['mensagem-sucesso'] = $mensagem;
+
+	}
+
+
+	function getMensagemSucesso() : ?string
+	{
+		if (isset($_SESSION['mensagem-sucesso'])) {
+			
+			return $_SESSION['mensagem-sucesso'];
+		}
+		
+		return null;
+
+	}
+
 	function setMensagemErro(string $mensagem) :void
 	{
 		
@@ -20,4 +39,18 @@
 		
 		return null;
 
+	}
+
+	function removeMensagemSucesso() : void
+	{
+		if (isset($_SESSION['mensagem-sucesso'])) {
+			return unset($_SESSION['mensagem-sucesso'])
+		}
+	}
+
+	function removeMensagemErro() : void
+	{
+		if (isset($_SESSION['mensagem-erro'])) {
+			return unset($_SESSION['mensagem-erro'])
+		}
 	}
--- servicos/servicoValidacao.php
+++ servicos/servicoValidacao.php
@@ -4,16 +4,16 @@ function validaNome(string $nome) : bool
 {
 
 	if(empty($nome)){
-		$_SESSION['mensagem-erro'] = "Preencha todos os dados, por gentileza!";
+		setMensagemErro("Preencha todos os dados, por gentileza!");
 		return false;
 	}
 	else if (strlen($nome) < 5) {
-		$_SESSION['mensagem-erro'] = "Digite o nome completo";
+		setMensagemErro("Digite o nome completo");
 		return false;
 	}
 
 	else if (strlen($nome) > 40) {
-		$_SESSION['mensagem-erro'] = "Nome muito extenso";
+		setMensagemErro("Nome muito extenso");
 		header('location: cad_compet.php');
         return false;
 	}
@@ -27,11 +27,11 @@ function validaIdade(string $idade) : bool
 {
 
 	if(empty($idade)){
-		$_SESSION['mensagem-erro'] = "Preencha todos os dados, por gentileza!";
+		setMensagemErro("Preencha todos os dados, por gentileza!");
 		return false;
 	}
 	else if(!is_numeric($idade)){
-		$_SESSION['mensagem-erro'] = "Idade invalida";
+		setMensagemErro("Idade invalida");
         return false;
 	}
 
